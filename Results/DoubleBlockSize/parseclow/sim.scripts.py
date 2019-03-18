014f62b76dc11c815980715971d2e176ffc38053
Sarabjeet Singh <sarabjeet.singh@iitgn.ac.in>
2019-03-18 14:12:07 +0530

    Cachelevel AsymWrites

---
+++ Results/DoubleBlockSize/parseclow/sim.scripts.py
@@ -0,0 +1,3 @@
+import sys
+sys.argv = [ "/home/sarabjeet/Sarabjeet/REBUT/DoubBlockSize/scripts/stop-by-icount.py", "20500000000:500000000" ]
+execfile("/home/sarabjeet/Sarabjeet/REBUT/DoubBlockSize/scripts/stop-by-icount.py")
---
+++ Results/DoubleSets/fluidanimate/sim.scripts.py
@@ -0,0 +1,3 @@
+import sys
+sys.argv = [ "/home/sarabjeet/Sarabjeet/REBUT/DoubSets/scripts/stop-by-icount.py", "2500000000:500000000" ]
+execfile("/home/sarabjeet/Sarabjeet/REBUT/DoubSets/scripts/stop-by-icount.py")
